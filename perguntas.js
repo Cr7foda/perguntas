03164e60d0a83f982a10de878269234eea02cf22
cristiano lindo <00001100759608sp@al.educacao.sp.gov.br>
2024-11-08 11:45:00 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Futebol',
+    'Qual é o melhor jogador do mundo?',
+    'O melhor jogador do mundo é o Cristiano Ronaldo'
 )
 
 criaCartao(
